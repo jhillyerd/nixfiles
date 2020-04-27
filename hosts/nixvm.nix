0aab6edb85206ebdfff7b6faed79b4939c81fb59
James Hillyerd <james@hillyerd.com>
2020-04-27 08:07:03 -0700

    nixvm: init

---
+++ hosts/nixvm.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./common.nix
+    ];
+
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    device = "/dev/sda";
+  };
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  powerManagement.enable = false;
+
+  networking.hostName = "nixvm";
+  networking.useDHCP = false;
+  networking.interfaces.ens33.useDHCP = true;
+  networking.firewall.enable = false;
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    dpi = 108;
+    layout = "us";
+    windowManager.i3.enable = true;
+  };
+
+  virtualisation.libvirtd.enable = true;
+}
+
